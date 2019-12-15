83a09dba9aae2bfcb1d18a215f22eb65316941fa
Mihai Babet <mihai.babet@gmail.com>
2019-12-15 10:35:25 +0200

    add eslint

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const Nightmare = require('nightmare')
+const cheerio = require('cheerio')
 const nightmare = Nightmare({ show: true })
 
-
 const run = async () => {
-  const result = await nightmare
-  .goto('https://www.bet365.com/#/AC/B1/C1/D13/E108/F16')
-  .wait('.cm-CouponModule')
-  .evaluate(() => document.querySelector('body').innerHTML)
-  .end()
-  console.log(result)
-}
+  const html = await nightmare
+    .goto('https://www.bet365.com/#/AC/B1/C1/D13/E108/F16')
+    .wait('.cm-CouponModule')
+    .evaluate(() => document.querySelector('body').innerHTML)
+    .end()
 
+  const $ = cheerio.load(html)
+}
 
 run()
