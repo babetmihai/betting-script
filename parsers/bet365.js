e237b9ebc438211421c401f101fb9ae66269173c
Mihai Babet <mihai.babet@gmail.com>
2019-12-21 17:06:23 +0200

    modify script

--- parsers/bet365.js
+++ parsers/bet365.js
@@ -1,5 +1,12 @@
 const Nightmare = require('nightmare')
-const nightmare = Nightmare({ show: false })
+const nightmare = Nightmare({
+  show: true,
+  switches: {
+    'proxy-server': 'http://82.77.55.46:8080',
+    'ignore-certificate-errors': true
+  },
+  waitTimeout: 1000000000
+})
 
 const links = [
   'https://www.bet365.com/#/AC/B1/C1/D13/E37628398/F2/', // premier league
@@ -8,7 +15,7 @@ const links = [
   'https://www.bet365.com/#/AC/B1/C1/D13/E108/F16' // europa elite
 ]
 
-module.exports = async () => {
+const getList = async () => {
   const lists = await nightmare
     .goto('https://www.bet365.com/#/AC/B1/C1/D13/E37628398/F2')
     .wait('.sl-CouponParticipantWithBookCloses_Name')
@@ -33,4 +40,47 @@ module.exports = async () => {
       }))
     })
     .flat()
+}
+
+module.exports = async () => {
+  const values = []
+  for (const index in [...Array(31)]) {
+    const item = await nightmare
+      .goto('https://www.bet365.com/#/AS/B1/')
+      .wait('.sm-CouponLink_Title ')
+      .wait(1000)
+      .evaluate((index) => {
+        try {
+          const button = document.querySelector(`
+            body 
+            > div:nth-child(1) 
+            > div > div.wc-PageView 
+            > div.wc-PageView_Main 
+            > div 
+            > div.wcl-CommonElementStyle_PrematchCenter.wc-SplashPage_CenterColumn 
+            > div.sm-SplashModule 
+            > div.gl-MarketGrid.gl-MarketGrid-paddingforlhs 
+            > div:nth-child(2) 
+            > div.gl-MarketGroup_Wrapper.sm-SplashMarketGroup_Container 
+            > div 
+            > div:nth-child(2) 
+            > div.sm-SplashMarketContainer_Expanded
+            > div:nth-child(${index + 1})
+          `)
+
+          button.click()
+        } catch (error) {
+          // nothing
+        }
+
+      }, index)
+      .wait('.gll-ParticipantOddsOnly_Odds')
+      .wait(1000)
+      .evaluate(() => document.body.innerHTML)
+
+    values.push(item)
+  }
+
+  nightmare.end()
+  return values
 }
