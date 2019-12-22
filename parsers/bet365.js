c0e6a0153ec29ccec85eda7cf3559e44cdd76cf3
Mihai Babet <mihai.babet@gmail.com>
2019-12-22 15:03:32 +0200

    parse all lists

--- parsers/bet365.js
+++ parsers/bet365.js
@@ -1,88 +1,89 @@
 const Nightmare = require('nightmare')
 const nightmare = Nightmare({
   show: true,
-  switches: {
-    'proxy-server': 'http://82.77.55.46:8080',
-    'ignore-certificate-errors': true
-  },
-  waitTimeout: 25000
+  // switches: {
+  //   'proxy-server': 'http://82.77.55.46:8080',
+  //   'ignore-certificate-errors': true
+  // },
+  waitTimeout: 5000
 })
+
+const contentQuery = `
+  body 
+  > div:nth-child(1) 
+  > div 
+  > div.wc-PageView 
+  > div.wc-PageView_Main 
+  > div 
+  > div.wcl-CommonElementStyle_PrematchCenter.wc-SplashPage_CenterColumn 
+  > div.sm-SplashModule 
+  > div.gl-MarketGrid.gl-MarketGrid-paddingforlhs 
+  > div:nth-child(2) 
+  > div.gl-MarketGroup_Wrapper.sm-SplashMarketGroup_Container
+  > div.gl-MarketGroupContainer
+`
 
 module.exports = async () => {
   try {
     const values = []
-    const items = await nightmare.goto('https://www.bet365.com/#/AS/B1/')
+    const groups = await nightmare.goto('https://www.bet365.com/#/AS/B1/')
       .wait('.sm-SplashMarket')
       .wait(1000)
-      .evaluate(() => {
-        const items = document.querySelector(`
-          body 
-            > div:nth-child(1) 
-            > div 
-            > div.wc-PageView 
-            > div.wc-PageView_Main 
-            > div 
-            > div.wcl-CommonElementStyle_PrematchCenter.wc-SplashPage_CenterColumn 
-            > div.sm-SplashModule 
-            > div.gl-MarketGrid.gl-MarketGrid-paddingforlhs 
-            > div:nth-child(2) 
-            > div.gl-MarketGroup_Wrapper.sm-SplashMarketGroup_Container
-            > div.gl-MarketGroupContainer
-            > div:nth-child(2)
-            > div:nth-child(2)
-          `)
-        return [...items.childNodes].map((element) => element.innerText)
-      })
-    for (const text of items) {
-      console.log(text)
-      try {
-        const item = await nightmare
-          .wait(1000)
-          .evaluate((text) => {
-            const buttons = document.querySelector(`
-              body 
-              > div:nth-child(1) 
-              > div 
-              > div.wc-PageView 
-              > div.wc-PageView_Main 
-              > div 
-              > div.wcl-CommonElementStyle_PrematchCenter.wc-SplashPage_CenterColumn 
-              > div.sm-SplashModule 
-              > div.gl-MarketGrid.gl-MarketGrid-paddingforlhs 
-              > div:nth-child(2) 
-              > div.gl-MarketGroup_Wrapper.sm-SplashMarketGroup_Container
-              > div.gl-MarketGroupContainer
-              > div:nth-child(2)
-              > div:nth-child(2)
-            `)
+      .evaluate(({ contentQuery }) => {
+        const groupContainer = document.querySelector(contentQuery)
 
-            const button = [...buttons.children].find((element) => element.innerText.includes(text))
-            button && button.click()
+        const groups = []
+        for (const element of [...groupContainer.childNodes]) {
+          try {
+            const name = element.querySelector('.sm-SplashMarket_Header').innerText.trim()
+            const open = !!element.querySelector('.sm-SplashMarketContainer_Expanded')
+            groups.push({ open, name })
+          } catch (error) {
+            // nothing
+          }
 
-          }, text)
-          .wait('.gll-MarketGroup_Wrapper')
-          .wait(1000)
-          .evaluate(() => {
-            const quotes = document.querySelector(`
-              body 
-              > div:nth-child(1) 
-              > div 
-              > div.wc-PageView 
-              > div.wc-PageView_Main 
-              > div 
-              > div.wcl-CommonElementStyle_PrematchCenter 
-              > div.cm-CouponModule 
-              > div 
-              > div.gll-MarketGroup.cm-CouponMarketGroup.cm-CouponMarketGroup_DropdownIsAvailable.cm-CouponMarketGroup_Open 
-              > div.gll-MarketGroup_Wrapper
-            `)
-            return quotes.innerHTML
-          })
+        }
+        return groups
+      }, { contentQuery })
 
-        values.push(item)
-        nightmare.back()
-      } catch (error) {
-      // nothing
+    for (const { open, name } of groups) {
+      if (!open) {
+        nightmare.evaluate(({ contentQuery, name }) => {
+          const groupContainer = document.querySelector(contentQuery)
+          const button = [...groupContainer.children]
+            .find((element) => element.querySelector('.sm-SplashMarket_Header').innerText.includes(name))
+          button.click()
+        }, { contentQuery, name })
+      }
+
+      await nightmare.wait(1500)
+      const categories = await nightmare.evaluate(({ contentQuery, name }) => {
+        const groupContainer = document.querySelector(contentQuery)
+        const listContainer = [...groupContainer.children]
+          .find((element) => element.querySelector('.sm-SplashMarket_Header').innerText.includes(name))
+          .querySelector('.sm-SplashMarketContainer_Expanded')
+
+        return [...listContainer.children].map((element) => element.innerText.trim())
+      }, { contentQuery, name })
+      await nightmare.wait(1500)
+
+      for (const category of categories) {
+        await nightmare.evaluate(({ contentQuery, name, category }) => {
+          const groupContainer = document.querySelector(contentQuery)
+          const listContainer = [...groupContainer.children]
+            .find((element) => element.querySelector('.sm-SplashMarket_Header').innerText.includes(name))
+            .querySelector('.sm-SplashMarketContainer_Expanded')
+          const button = [...listContainer.children]
+            .find((element) => element.innerText.includes(category))
+
+          button.click()
+        }, { contentQuery, name, category })
+
+        await nightmare.wait('.gl-MarketGrid.gl-MarketGrid-paddingforlhs')
+        await nightmare.wait(1500)
+
+        await nightmare.back()
+        await nightmare.wait(1000)
       }
     }
     await nightmare.end()
