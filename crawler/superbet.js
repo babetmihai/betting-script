b96ad714a1412806319cc6a6d7cc37dc0ce0c20c
Mihai Babet <mihai.babet@gmail.com>
2019-12-26 17:01:26 +0200

    rename variables

--- crawler/superbet.js
+++ crawler/superbet.js
@@ -9,36 +9,35 @@ module.exports  = async () => {
       .goto('https://www.superbet.ro/pariuri-sportive/astazi')
       .wait('.event-row-container')
       .wait(1000)
-    const data = []
+    const dataAcc = []
     while (true) {
-      const quotes = await nightmare.evaluate(() => {
+      const newData = await nightmare.evaluate(() => {
         let lastMatch
-        const acc = []
+        const newDataAcc = []
         const matches = document.querySelectorAll('.event-row-container')
         for (const match of matches) {
           const team1 = match.querySelector('.event-summary__competitors-team1').innerText
           const team2 = match.querySelector('.event-summary__competitors-team2').innerText
           const odds = [...match.querySelectorAll('.pick__click-buffer')]
-            .reduce((acc, element) => {
+            .reduce((oddAcc, element) => {
               const id = element.querySelector('.market.actionable').innerText
               const value = element.querySelector('.value.new.actionable').innerText
-              acc[id] = value
-              return acc
+              oddAcc[id] = value
+              return oddAcc
             }, {})
-          acc.push({ teams: `${team1} - ${team2}`, odds })
+          newDataAcc.push({ teams: `${team1} - ${team2}`, odds })
           lastMatch = match
         }
         lastMatch.scrollIntoView()
-        return acc
-
+        return newDataAcc
       })
 
-      if (_.isEqual(_.last(data), _.last(quotes))) break
-      data.push(...quotes)
+      if (_.isEqual(_.last(dataAcc), _.last(newData))) break
+      dataAcc.push(...newData)
       await nightmare.wait(1000)
     }
 
-    db.set('superbet', data.reduce((acc, { teams, odds }) => {
+    db.set('superbet', dataAcc.reduce((acc, { teams, odds }) => {
       const match = { id: getId(teams), teams, odds }
       acc[match.id] = match
       return acc
