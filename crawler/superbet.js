facf0e5c2814e2af6ca7bd30f89dbc4c34b10d7c
Mihai Babet <mihai.babet@gmail.com>
2019-12-26 16:49:22 +0200

    refactor data

--- crawler/superbet.js
+++ crawler/superbet.js
@@ -1,5 +1,7 @@
 const db = require('../db')
 const nightmare = require('../crawler')
+const { getId } = require('../utils')
+const _ = require('lodash')
 
 module.exports  = async () => {
   try {
@@ -10,37 +12,35 @@ module.exports  = async () => {
     const data = []
     while (true) {
       const quotes = await nightmare.evaluate(() => {
-        const data = []
-        let lastEvent
-        const events = document.querySelectorAll('.event-row-container')
-        for (const event of events) {
-          const team1 = event.querySelector('.event-summary__competitors-team1').innerText
-          const team2 = event.querySelector('.event-summary__competitors-team2').innerText
-          const odds = [...event.querySelectorAll('.pick__click-buffer')]
+        let lastMatch
+        const acc = []
+        const matches = document.querySelectorAll('.event-row-container')
+        for (const match of matches) {
+          const team1 = match.querySelector('.event-summary__competitors-team1').innerText
+          const team2 = match.querySelector('.event-summary__competitors-team2').innerText
+          const odds = [...match.querySelectorAll('.pick__click-buffer')]
             .filter((element) => ['X', '1', '2'].includes(element.querySelector('.market.actionable').innerText))
             .map((element) => element.querySelector('.value.new.actionable').innerText)
-
-          data.push({ teams: `${team1} - ${team2}`, odds })
-          lastEvent = event
+          acc.push({ teams: `${team1} - ${team2}`, odds })
+          lastMatch = match
         }
-        lastEvent.scrollIntoView()
-        return data
+        lastMatch.scrollIntoView()
+        return acc
 
       })
-      console.log(data[data.length - 1], quotes[quotes.length - 1])
-      if (data[data.length - 1] && data[data.length - 1].teams === quotes[quotes.length - 1].teams) {
-        data.push(...quotes)
-        break
-      } else {
-        data.push(...quotes)
-      }
+
+      if (_.isEqual(_.last(data), _.last(quotes))) break
+      data.push(...quotes)
       await nightmare.wait(1000)
     }
 
-    db.set('superbet', data)
+    db.set('superbet', data.reduce((acc, { teams, odds }) => {
+      const match = { id: getId(teams), teams, odds }
+      acc[match.id] = match
+      return acc
+    }, {}))
       .write()
   } catch (error) {
     console.log(error.message)
   }
 }
-
