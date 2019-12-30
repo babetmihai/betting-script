dff408deba9da96cdb4449530fcd4c33bc6cc9f1
Mihai Babet <mihai.babet@gmail.com>
2019-12-30 19:44:26 +0200

    filter matches

--- core/index.js
+++ core/index.js
@@ -1,4 +1,6 @@
 const db = require('../db')
+const stringSimilarity = require('string-similarity')
+const _ = require('lodash')
 
 const getMaxOdds = (...lists) => lists.map((list) => Math.max(...list))
 const getProfit = (...odds) => 1 - odds.reduce((acc, odd) => acc + 1 / odd, 0)
@@ -6,7 +8,25 @@ const getBets = (...odds) => odds.map((odd) => 1 / odd)
 
 
 const findArbitrage = async () => {
-  const data = db.get('data')
+  const data = db.get('data').value()
+
+  const matches = Object.keys(data)
+    .reduce((acc, key) => {
+      acc.push(...Object.values(data[key]).map(({ teams }) => teams))
+      return acc
+    }, [])
+    .sort()
+    .reduce((acc, name) => {
+      const previousName = _.last(acc)
+      if (!previousName || stringSimilarity.compareTwoStrings(previousName, name) < .6) {
+        acc.push(name)
+      }
+      return acc
+    }, [])
+
+  console.log(matches)
+  console.log(stringSimilarity.compareTwoStrings('ASV Hamm-Westfalen - Gummersbach Vfl',
+    'ASV Hamm-Westfalen - VfL Gummersbach'))
 }
 
 module.exports = {
